1cf25334abed11f62813dbf43b36ba8c8686577c
venkatasubbaiah <venky.kandagaddala@gmail.com>
2013-09-04 14:54:56 +0530

    intial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Wireframes::Application.config.secret_token = '7b5a24f991495c3a8d146c65312d53a7a31efdeb01a86af38aec0dcc4aa550230bc0a140ea1fb819719962498cbe2dfe06a365a929cb5a50e706479c2dbd658e'
